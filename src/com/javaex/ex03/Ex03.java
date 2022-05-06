1f1cea6ae2f3b43d27020f4720952b805a703110
jongbin-choi <kooma1123@naver.com>
2022-05-06 12:19:44 +0900

    2022-05-06 수정

--- src/com/javaex/ex03/Ex03.java
+++ src/com/javaex/ex03/Ex03.java
@@ -2,13 +2,11 @@ package com.javaex.ex03;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,13 +14,20 @@ public class Ex03 {
 
 	public static void main(String[] args)throws IOException {
 		List<Person> pList = new ArrayList<Person>();
-		
+		/*
 		//1. 파일을 읽어온다.
 		InputStream in = new FileInputStream("C:\\javaStudy\\file\\PhoneDB_원본.txt");
 		//2. 파일을 번역한다. in을 번역한다. 뭘로? UTF-8로
 		InputStreamReader isr = new InputStreamReader(in, "UTF-8"); 
 		//3. 번역된 파일을 프로그램에 보내준다.
 		BufferedReader br = new BufferedReader(isr);
+		*/
+		
+		//읽기 Stream
+		Reader rd = new FileReader("C:\\javaStudy\\file\\PhoneDB_원본.txt");
+		BufferedReader br = new BufferedReader(rd);
+		
+		
 		
 		while(true) {
 			String str = br.readLine();
@@ -30,9 +35,13 @@ public class Ex03 {
 				break;
 			}
 			String[] sArray = str.split(",");
-			
-			pList.add(new Person(sArray[0], sArray[1], sArray[2]));
+			String name = sArray[0];
+			String hp = sArray[1];
+			String company = sArray[2];
+			pList.add(new Person(name, hp, company));
 		}//while문 끝
+		
+		pList.add(new Person("황일영", "010-2221-2222", "02-123-4567"));
 		
 		for(Person p : pList) {
 			System.out.println("이름 :" + p.getName());
@@ -41,38 +50,30 @@ public class Ex03 {
 			System.out.println("");
 		}
 		
-		pList.add(new Person("황일영", "010-2221-2222", "02-123-4567"));
 		
+		//쓰기 Stream	
+		// Writer Stream은 사용하는 순간 백지됨. 그래서 내용 다 읽어오고 써야한다.	
+		Writer wt = new FileWriter("C:\\javaStudy\\file\\PhoneDB_원본.txt");
+		BufferedWriter bw = new BufferedWriter(wt);
 		
-		
-		
-		
+		/*
 		//1. 파일을 복사한다.
 		OutputStream out = new FileOutputStream("C:\\javaStudy\\file\\PhoneDB_원본.txt");
 		//2. 복사할 파일을 번역한다.
 		OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");//
 		//3. 프로그램에서 번역할 파일을 번역기에 보내준다.
 		BufferedWriter bw = new BufferedWriter(osw);
+		*/
 		
-		for(int i = 0; i < pList.size(); i++) {
-			bw.write("이름 : " + pList.get(i).getName());
-			bw.newLine();
-			bw.write("핸드폰 : " + pList.get(i).getHp());
-			bw.newLine();
-			bw.write("회사 : " + pList.get(i).getCompany());
+	
+		
+		for(Person person : pList) {
+			//saveStr에 pList를 담는다. (pList는 person변수에 담음)
+			//저장할때에는 원본 파일을 바꾸지않는다. 그래야 위에서 입력한 값들이 동일하게 읽어올수있다. 
+			String saveStr = person.getName() + "," + person.getHp() + "," + person.getCompany();
+			bw.write(saveStr);
 			bw.newLine();
 		}
-		System.out.println("=================================");	
-		for(Person p : pList) {
-			System.out.println("이름 :" + p.getName());
-			System.out.println("핸드폰 :" + p.getHp());
-			System.out.println("회사 :" + p.getCompany());
-			System.out.println("");
-		}
-		
-		
-		
-		
 		
 		
 		
